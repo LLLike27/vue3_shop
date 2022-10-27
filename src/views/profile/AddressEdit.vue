5654732d387c042ee4364d4db5ffbb1a3dbba3be
LLLike27 <lushuaizong@gmail.com>
2022-10-28 00:12:41 +0800

    vue3_shop three commint

--- src/views/profile/AddressEdit.vue
+++ src/views/profile/AddressEdit.vue
@@ -1,178 +1,191 @@
 <template>
-    <div class="address-edit-box">
-      <nav-bar>
-          <template v-slot:default>{{title}}</template>
-      </nav-bar>
+  <div class="address-edit-box">
+    <nav-bar>
+      <template v-slot:default>{{ title }}</template>
+    </nav-bar>
 
-        <van-address-edit class="edit"
-                :area-list="areaList"
-                :address-info="addressInfo"
-                :show-delete="type == 'edit'"
-                show-set-default
-                show-search-result
-                :search-result="searchResult"
-                :area-columns-placeholder="['请选择', '请选择', '请选择']"
-                @save="onSave"
-                @delete="onDelete"
-        />
-    </div>
+    <van-address-edit
+      class="edit"
+      :area-list="areaList"
+      :address-info="addressInfo"
+      :show-delete="type == 'edit'"
+      show-set-default
+      show-search-result
+      :search-result="searchResult"
+      :area-columns-placeholder="['请选择', '请选择', '请选择']"
+      @save="onSave"
+      @delete="onDelete"
+    />
+  </div>
 </template>
 
 <script>
-    import NavBar from "@/components/common/navbar/NavBar";
-    import { ref,onMounted,reactive,toRefs,computed } from 'vue';
-    import {tdist} from "@/utils/address";
-    import {useRoute,useRouter} from 'vue-router'
-    import {addAddress,editAddress,deleteAddress,getAddressDetail} from "@/network/address";
-    import { Toast } from 'vant';
-    import 'vant/es/toast/style';
-    export default {
-        name: "AddressEdit",
-        components: {NavBar},
-        setup() {
-            const route = useRoute()
-            const router = useRouter()
-            const state = reactive({
-                areaList:{//地区列表
-                    province_list:{},
-                    city_list:{},
-                    county_list:{}
-                },
-                searchResult: [],//详细地址搜索结果待补全
-                addressInfo:{},//地址信息初始值
-                title:'',
-                type:'add',
-                addressId: '',
-            })
+import NavBar from "@/components/common/navbar/NavBar";
+import { ref, onMounted, reactive, toRefs, computed } from "vue";
+import { tdist } from "@/utils/address";
+import { useRoute, useRouter } from "vue-router";
+import {
+  addAddress,
+  editAddress,
+  deleteAddress,
+  getAddressDetail,
+} from "@/network/address";
+import { Toast } from "vant";
+import "vant/es/toast/style";
+export default {
+  name: "AddressEdit",
+  components: { NavBar },
+  setup() {
+    const route = useRoute();
+    const router = useRouter();
+    const state = reactive({
+      areaList: {
+        //地区列表
+        province_list: {},
+        city_list: {},
+        county_list: {},
+      },
+      searchResult: [], //详细地址搜索结果待补全
+      addressInfo: {}, //地址信息初始值
+      title: "",
+      type: "add",
+      addressId: "",
+    });
 
-            onMounted(()=>{
-                // 省市区 列表 构造出来
-                let _province_list={};
-                let _city_list={};
-                let _county_list={}
+    onMounted(() => {
+      // 省市区 列表 构造出来
+      let _province_list = {};
+      let _city_list = {};
+      let _county_list = {};
 
-                tdist.getLev1().forEach(p=>{
-                    _province_list[p.id] = p.text;
-                    tdist.getLev2(p.id).forEach(c=>{
-                        _city_list[c.id] = c.text;
-                        tdist.getLev3(c.id).forEach(q=>{
-                            _county_list[q.id] = q.text;
-                        })
-                    })
-                })
-                state.areaList.province_list = _province_list;
-                state.areaList.city_list = _city_list;
-                state.areaList.county_list = _county_list;
-                // 接收参数
-                const { type,addressId} = route.query;
+      tdist.getLev1().forEach((p) => {
+        _province_list[p.id] = p.text;
+        tdist.getLev2(p.id).forEach((c) => {
+          _city_list[c.id] = c.text;
+          tdist.getLev3(c.id).forEach((q) => {
+            _county_list[q.id] = q.text;
+          });
+        });
+      });
+      state.areaList.province_list = _province_list;
+      state.areaList.city_list = _city_list;
+      state.areaList.county_list = _county_list;
+      // 接收参数
+      const { type, addressId } = route.query;
 
-                state.type = type;
-                state.addressId = addressId;
-                // console.log(state.addressId)
-                if (type == 'edit'){
-                    getAddressDetail(addressId).then(res=>{
-                        const addressDetail = res.data;//全部数据 省市区
-                        let _areaCode = ''
-                        const province = tdist.getLev1();//省份
-
-                        Object.entries(state.areaList.county_list).forEach(([id,text])=>{
-                            // console.log('----'+id,'====='+text)
-                            //先找出当前对应的区
-                            if (text == addressDetail.county) {
-                                // 通过区id前面两个与省前面两个id相同 找区对应的省份
-                                const provinceIndex = province.findIndex(item => item.id.substr(0, 2) == id.substr(0, 2))
-                                // 找到区对应的几个市区
-                                const cityIndex = Object.entries(state.areaList.city_list).filter(
-                                    ([cityId,city])=> cityId.substr(0,4) == id.substr(0,4)
-                                )[0]
-                                //对比找到的省份和接口返回的省份是否相等，因为有一些区会重名
-                                if (province[provinceIndex].text == addressDetail.province && cityIndex[1] == addressDetail.city){
-                                    _areaCode = id;
-                                }
-                            }
-                        })
-                        state.addressInfo ={
-                            name:addressDetail.name,
-                            tel:addressDetail.phone,
-                            province: addressDetail.province,
-                            city:addressDetail.city,
-                            county:addressDetail.county,
-                            areaCode:_areaCode,
-                            addressDetail:addressDetail.address,
-                            isDefault: !!addressDetail.is_default
-                        }
-                    })
-                }
-            })
-            const title =computed(()=>{
-                return state.type == 'add' ? '新增地址' : '编辑地址'
-            })
-            const searchResult = ref([]);
-
-            //保存
-            const onSave = (content) => {
-                //数据模型 把表单内容赋给默认信息
-                const params = {
-                    name:content.name,
-                    phone:content.tel,
-                    province:content.province,
-                    city:content.city,
-                    county:content.county,
-                    address:content.addressDetail,
-                    is_default:content.isDefault ? 1 : 0
-                }
-                //根据传过来的type 分别调用不同的接口
-                if (state.type =='edit'){
-                    //调用接口编辑地址
-                    editAddress(state.addressId,params);
-                }else if(state.type ='add') {
-                    //调用接口添加数据
-                    addAddress(params);
-                }
-                Toast('保存成功');
-                setTimeout(()=>{
-                    router.back();
-                }, 1000)
+      state.type = type;
+      state.addressId = addressId;
+      // console.log(state.addressId)
+      if (type == "edit") {
+        getAddressDetail(addressId).then((res) => {
+          const addressDetail = res.data; //全部数据 省市区
+          // console.log(addressDetail)
+          let _areaCode = "";
+          const province = tdist.getLev1(); //省份
+          // console.log(province)
+          Object.entries(state.areaList.county_list).forEach(([id, text]) => {
+            // console.log('----'+id,'====='+text)
+            //先找出当前对应的区
+            if (text == addressDetail.county) {
+              // 通过区id前面两个与省前面两个id相同 找区对应的省份
+              const provinceIndex = province.findIndex(
+                (item) => item.id.substr(0, 2) == id.substr(0, 2)
+              );
+              // console.log(provinceIndex)
+              // 找到区对应的几个市区
+              const cityIndex = Object.entries(state.areaList.city_list).filter(
+                ([cityId, city]) => cityId.substr(0, 4) == id.substr(0, 4)
+              )[0];
+              //对比找到的省份和接口返回的省份是否相等，因为有一些区会重名
+              if (
+                province[provinceIndex].text == addressDetail.province &&
+                cityIndex[1] == addressDetail.city
+              ) {
+                _areaCode = id;
+              }
             }
-            //删除
-            const onDelete = () =>{
-                deleteAddress(state.addressId).then(res=>{
-                    Toast('删除成功');
-                    setTimeout(()=>{
-                        router.back();
-                    }, 1000)
-                })
-            }
+          });
+          state.addressInfo = {
+            name: addressDetail.name,
+            tel: addressDetail.phone,
+            province: addressDetail.province,
+            city: addressDetail.city,
+            county: addressDetail.county,
+            areaCode: _areaCode,
+            addressDetail: addressDetail.address,
+            isDefault: !!addressDetail.is_default,
+          };
+        });
+      }
+    });
+    const title = computed(() => {
+      return state.type == "add" ? "新增地址" : "编辑地址";
+    });
+    const searchResult = ref([]);
+    //保存
+    const onSave = (content) => {
+      //数据模型 把表单内容赋给默认信息
+      const params = {
+        name: content.name,
+        phone: content.tel,
+        province: content.province,
+        city: content.city,
+        county: content.county,
+        address: content.addressDetail,
+        is_default: content.isDefault ? 1 : 0,
+      };
+      //根据传过来的type 分别调用不同的接口
+      if (state.type == "edit") {
+        //调用接口编辑地址
+        editAddress(state.addressId, params);
+      } else if ((state.type = "add")) {
+        //调用接口添加数据
+        addAddress(params);
+      }
+      Toast("保存成功");
+      setTimeout(() => {
+        router.back();
+      }, 1000);
+    };
+    //删除
+    const onDelete = () => {
+      deleteAddress(state.addressId).then((res) => {
+        Toast("删除成功");
+        setTimeout(() => {
+          router.back();
+        }, 1000);
+      });
+    };
 
-            return {
-                ...toRefs(state),
-                onSave,
-                searchResult,
-                title,
-                onDelete
-            };
-        },
-    }
+    return {
+      ...toRefs(state),
+      onSave,
+      searchResult,
+      title,
+      onDelete,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-    .edit {
-        .van-field__body {
-            textarea {
-                height: 26px!important;
-            }
-        }
+.edit {
+  .van-field__body {
+    textarea {
+      height: 26px !important;
     }
-    .address-edit-box {
-        margin-top: 44px;
-        .van-address-edit {
-            .van-button--danger {
-                background: var(--color-tint);
-                border-color: var(--color-tint);
-            }
-            .van-switch--on {
-                background:var(--color-tint);
-            }
-        }
+  }
+}
+.address-edit-box {
+  margin-top: 44px;
+  .van-address-edit {
+    .van-button--danger {
+      background: var(--color-tint);
+      border-color: var(--color-tint);
     }
+    .van-switch--on {
+      background: var(--color-tint);
+    }
+  }
+}
 </style>
--- src/views/shopcart/ShopCart.vue
+++ src/views/shopcart/ShopCart.vue
@@ -1,240 +1,280 @@
 <template>
-    <div>
-        <nav-bar>
-            <template v-slot:default>购物车</template>
-        </nav-bar>
-        <div class="cart-box">
-            <div class="cart-body">
-                    <van-checkbox-group @change="groupChange" v-model="result" ref="checkboxGroup">
-                        <van-swipe-cell :right-width="50"
-                        v-for="(item,index) in list" :key="index">
-                            <div class="good-item">
-                                <van-checkbox :name="item.id" />
-                                <div class="good-img"><img :src="item.goods.cover_url" alt=""></div>
-                                <div class="good-desc">
-                                    <div class="good-title">
-                                        <span>{{item.goods.title}}</span>
-                                        <span>x{{item.goods.stock}}</span>
-                                    </div>
-                                    <div class="good-btn">
-                                        <div class="price"><small>￥</small>{{item.goods.price + '.00'}}</div>
-                                        <van-stepper integer async-change @change="onChange" :min="1" :max="item.goods.stock" :name="item.id" :model-value="item.num"/>
-                                    </div>
-                                </div>
-                            </div>
-                            <template #right>
-                                <van-button
-                                        square
-                                        icon="delete"
-                                        type="danger"
-                                        class="delete-button"
-                                        @click="deleteGood(item.id)"
-                                />
-                            </template>
-                        </van-swipe-cell>
-                    </van-checkbox-group>
+  <div>
+    <nav-bar>
+      <template v-slot:default>购物车</template>
+    </nav-bar>
+    <div class="cart-box">
+      <div class="cart-body">
+        <van-checkbox-group
+          @change="groupChange"
+          v-model="result"
+          ref="checkboxGroup"
+        >
+          <van-swipe-cell
+            :right-width="50"
+            v-for="(item, index) in list"
+            :key="index"
+          >
+            <div class="good-item">
+              <van-checkbox :name="item.id" />
+              <div class="good-img">
+                <img :src="item.goods.cover_url" alt="" />
+              </div>
+              <div class="good-desc">
+                <div class="good-title">
+                  <span>{{ item.goods.title }}</span>
+                  <span>x{{ item.goods.stock }}</span>
+                </div>
+                <div class="good-btn">
+                  <div class="price">
+                    <small>￥</small>{{ item.goods.price + ".00" }}
+                  </div>
+                  <van-stepper
+                    integer
+                    async-change
+                    @change="onChange"
+                    :min="1"
+                    :max="item.goods.stock"
+                    :name="item.id"
+                    :model-value="item.num"
+                  />
+                </div>
+              </div>
             </div>
-            <van-submit-bar class="submit-all" :price="total * 100" @submit="onSubmit" button-text="结算">
-                <van-checkbox @click="allCheck" v-model:checked="checkAll">全选</van-checkbox>
-            </van-submit-bar>
-            <div class="empty" v-if="!list.length">
-                <img class="empty-cart" src="~assets/images/empty-car.png" alt="空购物车">
-                <div class="title">购物车空空如也</div>
-                <van-button round color="#1baeae" type="primary" block @click="goTo">前往选购</van-button>
-            </div>
-        </div>
+            <template #right>
+              <van-button
+                square
+                icon="delete"
+                type="danger"
+                class="delete-button"
+                @click="deleteGood(item.id)"
+              />
+            </template>
+          </van-swipe-cell>
+        </van-checkbox-group>
+      </div>
+      <van-submit-bar
+        class="submit-all"
+        :price="total * 100"
+        @submit="onSubmit"
+        button-text="结算"
+      >
+        <van-checkbox @click="allCheck" v-model:checked="checkAll"
+          >全选</van-checkbox
+        >
+      </van-submit-bar>
+      <div class="empty" v-if="!list.length">
+        <img
+          class="empty-cart"
+          src="~assets/images/empty-car.png"
+          alt="空购物车"
+        />
+        <div class="title">购物车空空如也</div>
+        <van-button round color="#1baeae" type="primary" block @click="goTo"
+          >前往选购</van-button
+        >
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import NavBar from "@/components/common/navbar/NavBar";
-    import {getCart,deleteCartItem,modifyCart,checkedCart} from "@/network/cart";
-    import {useStore} from 'vuex'
-    import {useRouter} from 'vue-router'
-    import {onMounted,ref,reactive,toRefs,computed} from 'vue'
-    import {Toast} from 'vant'
-    import 'vant/es/toast/style';
-    export default {
-        name: "ShopCart",
-        components: {NavBar},
-        setup(){
-            const router = useRouter();
-            const store =useStore();
-            // 数据模型， 状态
-            const state = reactive({
-                list:[],
-                result:[],//id数值
-                checkAll:true
-            })
+import NavBar from "@/components/common/navbar/NavBar";
+import {
+  getCart,
+  deleteCartItem,
+  modifyCart,
+  checkedCart,
+} from "@/network/cart";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { onMounted, ref, reactive, toRefs, computed } from "vue";
+import { Toast } from "vant";
+import "vant/es/toast/style";
+export default {
+  name: "ShopCart",
+  components: { NavBar },
+  setup() {
+    const router = useRouter();
+    const store = useStore();
+    // 数据模型， 状态
+    const state = reactive({
+      list: [],
+      result: [], //id数值
+      checkAll: true,
+    });
 
-            // 初始化购物车数据
-            const init = ()=>{
-                Toast.loading({message:'加载中...', forbidClick:true});
-                getCart('include=goods').then(res=>{
-                    state.list = res.data.data;
-                    state.result = res.data.data.filter(n=>n.is_checked == 1).map(item =>item.id);
-                    Toast.clear();
-                })
-            }
-            // 通过计算属性 计算总价
-            const total = computed(()=>{
-                let sum=0;
-                state.list.filter(item=> state.result.includes(item.id))
-                    .forEach(item =>{
-                        sum += parseInt(item.num) * parseFloat(item.goods.price);
-                    })
-                return sum;
-            })
-            // 异步改变购物车数量
-            const onChange = (value,detail)=>{
-                //value数量,detail.name=商品id
-                Toast.loading({message:'修改中...', forbidClick:true});
-                console.log('----'+value,'----'+detail)
-                modifyCart(detail.name,{num:value}).then(res=>{
-                    // //将在onMounted中的数据模型， 状态里面list也要更改
-                    state.list.forEach(item =>{
-                        if (item.id ==detail.name){
-                            item.num = value;
-                        }
-                    })
-                })
-                Toast.clear();
-            }
-            // 复选框改变处理
-            const groupChange =(result)=>{
-                if (result.length == state.list.length){
-                    state.checkAll = true;
-                }else {
-                    state.checkAll = false;
-                }
+    // 初始化购物车数据
+    const init = () => {
+      Toast.loading({ message: "加载中...", forbidClick: true });
+      getCart("include=goods").then((res) => {
+        state.list = res.data.data;
+        state.result = res.data.data
+          .filter((n) => n.is_checked == 1)
+          .map((item) => item.id);
+        Toast.clear();
+      });
+    };
+    // 通过计算属性 计算总价
+    const total = computed(() => {
+      let sum = 0;
+      state.list
+        .filter((item) => state.result.includes(item.id))
+        .forEach((item) => {
+          sum += parseInt(item.num) * parseFloat(item.goods.price);
+        });
+      return sum;
+    });
+    // 异步改变购物车数量
+    const onChange = (value, detail) => {
+      //value数量,detail.name=商品id
+      Toast.loading({ message: "修改中...", forbidClick: true });
+      console.log("----" + value, "----" + detail);
+      modifyCart(detail.name, { num: value }).then((res) => {
+        // 将在onMounted中的数据模型， 状态里面list也要更改
+        state.list.forEach((item) => {
+          if (item.id == detail.name) {
+            item.num = value;
+          }
+        });
+      });
+      Toast.clear();
+    };
+    // 复选框改变处理
+    const groupChange = (result) => {
+      if (result.length == state.list.length) {
+        state.checkAll = true;
+      } else {
+        state.checkAll = false;
+      }
 
-                ({cart_ids:result})
-            }
-            // 全选框改变处理
-            const allCheck = ()=>{
-                if (!state.checkAll){
-                    state.result = state.list.map(item=>item.id);//全选框选中的商品id
-                    state.checkAll = true
-                    // console.log(state.result)
-                }
-                else {
-                    state.result = []//全选框选中的商品id为空
-                    state.checkAll=false
-                }
-            }
-            //前往选购
-            const goTo = ()=>{
-                router.push({path:'/home'});
-            }
-            //删除购物车商品
-            const deleteGood = (id)=>{
-                // console.log(id)
-                deleteCartItem(id).then(res=>{
-                    init(); //重新初始化
-                    store.dispatch('updateCart')
-                })
-            }
-            //结算 创建订单
-            const onSubmit = () => {
-                if(state.result.length == 0) {
-                    Toast.fail("请选择商品进行结算");
-                    return;
-                }else{
-                    router.push({path:'/createorder'});
-                }
-            }
-            onMounted(()=>{
-                init();
-            })
-            return {
-                ...toRefs(state),
-                goTo,
-                deleteGood,
-                onChange,
-                groupChange,
-                allCheck,
-                total,
-                onSubmit
-            }
-        }
-    }
+      // 改变数据表中选中状态
+      checkedCart({ cart_ids: result });
+    };
+    // 全选框改变处理
+    const allCheck = () => {
+      if (!state.checkAll) {
+        state.result = state.list.map((item) => item.id); //全选框选中的商品id
+        state.checkAll = true;
+        // console.log(state.result)
+      } else {
+        state.result = []; //全选框选中的商品id为空
+        state.checkAll = false;
+      }
+    };
+    //前往选购
+    const goTo = () => {
+      router.push({ path: "/home" });
+    };
+    //删除购物车商品
+    const deleteGood = (id) => {
+      // console.log(id)
+      deleteCartItem(id).then((res) => {
+        init(); //重新初始化
+        store.dispatch("updateCart");
+      });
+    };
+    //结算 创建订单
+    const onSubmit = () => {
+      if (state.result.length == 0) {
+        Toast.fail("请选择商品进行结算");
+        return;
+      } else {
+        router.push({ path: "/createorder" });
+      }
+    };
+    onMounted(() => {
+      init();
+    });
+    return {
+      ...toRefs(state),
+      goTo,
+      deleteGood,
+      onChange,
+      groupChange,
+      allCheck,
+      total,
+      onSubmit,
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
-    .cart-box {
-        .cart-body {
-            margin: 60px 0 100px 0;
-            padding-left: 10px;
-            .good-item {
-                display: flex;
-                .good-img {
-                    img {
-                        width:100px;
-                        height:auto;
-                    }
-                }
-                .good-desc {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: space-between;
-                    flex: 1;
-                    padding: 20px;
-                    .good-title {
-                        display: flex;
-                        justify-content: space-between;
-                    }
-                    .good-btn {
-                        display: flex;
-                        justify-content: space-between;
-                        .price {
-                            font-size: 16px;
-                            color: red;
-                            line-height: 28px;
-                        }
-                        .van-icon-delete {
-                            font-size: 20px;
-                            margin-top: 4px;
-                        }
-                    }
-                }
-            }
-            .delete-button {
-                width: 50px;
-                height: 100%;
-            }
+.cart-box {
+  .cart-body {
+    margin: 60px 0 100px 0;
+    padding-left: 10px;
+    .good-item {
+      display: flex;
+      .good-img {
+        img {
+          width: 100px;
+          height: auto;
         }
-        .empty {
-            width: 50%;
-            margin: 0 auto;
-            text-align: center;
-            margin-top: 200px;
-            .empty-cart {
-                width: 150px;
-                margin-bottom: 20px;
-            }
-            .van-icon-smile-o {
-                font-size: 50px;
-            }
-            .title {
-                font-size: 16px;
-                margin-bottom: 20px;
-            }
+      }
+      .good-desc {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        flex: 1;
+        padding: 20px;
+        .good-title {
+          display: flex;
+          justify-content: space-between;
         }
-        .submit-all {
-            margin-bottom: 60px;
-            .van-checkbox {
-                margin-left: 0px
-            }
-            .van-submit-bar__text {
-                margin-right: 10px
-            }
-            .van-submit-bar__button {
-                background:red;
-            }
+        .good-btn {
+          display: flex;
+          justify-content: space-between;
+          .price {
+            font-size: 16px;
+            color: red;
+            line-height: 28px;
+          }
+          .van-icon-delete {
+            font-size: 20px;
+            margin-top: 4px;
+          }
         }
-        .van-checkbox__icon--checked .van-icon {
-            background-color:red;
-            border-color: red;
-        }
+      }
     }
+    .delete-button {
+      width: 50px;
+      height: 100%;
+    }
+  }
+  .empty {
+    width: 50%;
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 200px;
+    .empty-cart {
+      width: 150px;
+      margin-bottom: 20px;
+    }
+    .van-icon-smile-o {
+      font-size: 50px;
+    }
+    .title {
+      font-size: 16px;
+      margin-bottom: 20px;
+    }
+  }
+  .submit-all {
+    margin-bottom: 60px;
+    .van-checkbox {
+      margin-left: 0px;
+    }
+    .van-submit-bar__text {
+      margin-right: 10px;
+    }
+    .van-submit-bar__button {
+      background: red;
+    }
+  }
+  .van-checkbox__icon--checked .van-icon {
+    background-color: red;
+    border-color: red;
+  }
+}
 </style>
